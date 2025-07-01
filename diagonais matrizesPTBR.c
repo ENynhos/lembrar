4e114e2dbe08f47c0d9c445c635431d3bbed780b
ENynhos <lucaspompermayerribeiro@gmail.com>
2025-07-01 17:10:15 -0300

    Update and rename diagonais matrizes.c to diagonais matrizesPTBR.c

--- diagonais matrizesPTBR.c
+++ diagonais matrizesPTBR.c
@@ -1,28 +1,28 @@
 //acima da diagonal principal
 for(int i = 0; i < lin; i++){
             for(int j = (i + 1); j < col; j++){
-                s += m[i][j];
-                counter++;
+                s += m[i][j];//soma dos elementos lidos
+                counter++;//a quantidade de elementos que foram lidos
                 }
             }
 //abaixo da diagonal principal
  for(int i = 0; i < lin; i++){
             for(int j = 0; j < i; j++){
-                s += m[i][j];
-                counter++;
+                s += m[i][j];//soma dos elementos lidos
+                counter++;//a quantidade de elementos que foram lidos
             }
        }
 //acima diagonal secundaria 
 for(int i = 0; i < lin; i++){
             for(int j = 0; j < col - (i+1) ; j++){
-                s += m[i][j];
-                counter++;
+                s += m[i][j];//soma dos elementos lidos
+                counter++;//a quantidade de elementos que foram lidos
                 }
             }
 //abaixo da diagonal secundária
 for(int i = 0; i < lin; i++){
             for(int j = (col - i); j < lin ; j++){
-                s += m[i][j];
-                counter++;
+                s += m[i][j];//soma dos elementos lidos
+                counter++;//a quantidade de elementos que foram lidos
                 }
             }
