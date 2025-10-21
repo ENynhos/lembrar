6d393ab1aef7d6bb5cf6a1d7ba59250b3ebd2d87
ENynhos <lucaspompermayerribeiro@gmail.com>
2025-10-21 16:57:45 -0300

    Create BinTree.h

---
+++ BinTrees/BinTree.h
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+
+#define CLEAR "\033[0;0;0m"
+#define RESET "\033[0m"
+#define YELLOW_GREEN_BOLD "\033[93;42;1m"
+
+
+typedef unsigned Tdado;
+
+typedef struct TreeNode
+{
+	Tdado info;
+	struct TreeNode *left;
+	struct TreeNode *right;
+} TreeNode;
+
+//função de inserção
+TreeNode* insertBintree(Tdado x, TreeNode *bt);
+
+//função de remoção por merge(promoção)
+TreeNode* deleteByMerge(TreeNode *bt, Tdado x);
+
+
+//valor mínmo da árvore
+Tdado minTree(TreeNode *bt);
+
+//remoção por cópia
+TreeNode* deleteByCopy(TreeNode *bt, Tdado x);
+
+//impressão
+void printBinTree(const TreeNode *bt, int level);
+
+//apaga a arvore
+void freeBinTree(TreeNode* bt);
